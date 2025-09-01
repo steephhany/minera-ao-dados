2c3d741e9f7e61dc2ff448e6d6dbbcaefad1dd3b
steephhany <stephany.alves.reis@escola.pr.gov.br>
2025-09-01 09:53:57 -0300

    Delete Nova pasta directory

--- Nova pasta/informaçõesGlobal.js
+++
@@ -1,8 +0,0 @@
-const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
-
-async function vizualizarinformaçõesGlobal() {
-const res = await fetch(url)
-const dados =await res.json()
-console.log(dados);
-}
-visualizarinformaçõesGlobal();
